04f6a0a9a67f736f65d3d2a6c74b29075863fdf0
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2021-03-09 18:57:57 -0500

    :bug::recycle: Refactor the pipe to get the users with the role correctly

--- src/app/modules/auth/users/pipes/users-with-role/users-with-role.pipe.ts
+++ src/app/modules/auth/users/pipes/users-with-role/users-with-role.pipe.ts
@@ -2,7 +2,7 @@ import { Inject, Pipe, PipeTransform } from '@angular/core';
 import { RefreshRoles, REFRESH_ROLES } from '@core/providers/refresh.provider';
 import { UserResponse } from '@modules/auth/models/authentication';
 import { RoleResponse } from '@modules/auth/models/role';
-import { combineLatest, from, Observable, of } from 'rxjs';
+import { from } from 'rxjs';
 import { filter, map, mergeAll, mergeMap, take, toArray } from 'rxjs/operators';
 
 export interface UserWithRole extends UserResponse {
@@ -13,33 +13,21 @@ export interface UserWithRole extends UserResponse {
   name: 'usersWithRole'
 })
 export class UsersWithRolePipe implements PipeTransform {
-  private readonly roles$: Observable<RoleResponse[]>;
-
-  constructor(@Inject(REFRESH_ROLES) private refresh: RefreshRoles) {
-    this.roles$ = this.refresh.data$;
-  }
+  constructor(@Inject(REFRESH_ROLES) private refresh: RefreshRoles) { }
 
   transform(users: UserResponse[]) {
     this.refresh.dispatch();
-    const usersWithRole$ = from(users).pipe(
-      mergeMap(user => combineLatest([
-        of(user),
-        this.roleById(user.roleId)
-      ])),
-      map(([ user, role ]) => ({ ...user, role })),
+    const users$ = from(users);
+    const usersWithRole$ = this.refresh.data$.pipe(
+      take(1),
+      mergeAll(),
+      mergeMap(role => users$.pipe(
+        filter(({ roleId }) => roleId === role.id),
+        map(user => ({ ...user, role }))
+      )),
       toArray<UserWithRole>()
     );
 
     return usersWithRole$;
-  }
-
-  private roleById(roleId: number) {
-    const role$ = this.roles$.pipe(
-      take(1),
-      mergeAll(),
-      filter(({ id }) => id === roleId)
-    );
-
-    return role$;
   }
 }
